ed67453f83139ce2a53571a9e89abc20bf8c0e75
mattdegods <matthewcpfeifer@gmail.com>
2023-12-09 00:09:07 -0500

    vault close and testing

--- tests/anchor-vault-2.ts
+++ tests/anchor-vault-2.ts
@@ -1,16 +1,70 @@
 import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
+import { Program, BN } from "@coral-xyz/anchor";
 import { AnchorVault2 } from "../target/types/anchor_vault_2";
+import {
+  Keypair,
+  LAMPORTS_PER_SOL,
+  SystemProgram,
+  PublicKey,
+} from "@solana/web3.js";
 
-describe("anchor-vault-2", () => {
+describe("anchor-vault", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.AnchorVault2 as Program<AnchorVault2>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  const connection = anchor.getProvider().connection;
+
+  const signer = Keypair.generate();
+
+  const vault = PublicKey.findProgramAddressSync(
+    [Buffer.from("vault"), signer.publicKey.toBuffer()],
+    program.programId
+  )[0];
+
+  const confirm = async (signature: string) => {
+    const block = await connection.getLatestBlockhash();
+    await connection.confirmTransaction({ signature, ...block });
+    return signature;
+  };
+
+  const log = async (signature: string): Promise<string> => {
+    console.log(`Your transaction signature: https://explorer.solana.com/transaction/${signature}?
+    cluster=custom&customUrl=${connection.rpcEndpoint}`);
+    return signature;
+  };
+
+  it("Airdrop", async () => {
+    await connection
+      .requestAirdrop(signer.publicKey, LAMPORTS_PER_SOL * 10)
+      .then(confirm)
+      .then(log);
+  });
+
+  it("Deposit", async () => {
+    const tx = await program.methods
+      .deposit(new BN(LAMPORTS_PER_SOL))
+      .accounts({
+        owner: signer.publicKey,
+        vault,
+        systemProgram: SystemProgram.programId,
+      })
+      .rpc()
+      .then(confirm)
+      .then(log);
+  });
+
+  it("Close", async () => {
+    const tx = await program.methods
+      .close()
+      .accounts({
+        owner: signer.publicKey,
+        vault,
+        systemProgram: SystemProgram.programId,
+      })
+      .rpc()
+      .then(confirm)
+      .then(log);
   });
 });
